b4f017b53f0fb52299a410bbd419fa60ae9f926e
vladyslav <vladyslav.nestorko@justanswer.com>
2026-03-24 23:16:28 +0200

    1

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl({
-    name: 'test',
-    domains: ['*.custom.com'],
-    certDir: '/Users/.../.devServer/cert'
-  })],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
